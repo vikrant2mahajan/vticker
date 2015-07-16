bdf39b3232c0d1fd91b2b8d8d643dc699114e677
vikrant <vikrant_mahajan@ezeiatech.om>
2015-07-16 23:37:27 +0530

    vticker angular

--- angular.vticker.js
+++ angular.vticker.js
@@ -1,17 +1,19 @@
 /* --- Made by Vikrant Mahajan and licensed under MIT license --- */
 
 var app = angular.module('vticker', []);
-app.directive('ticker', function(){
+app.directive('ticker', function($timeout){
   return {
     restrict: 'E',
     scope:{
       ticks:"="
     },
-    template:"<ul><li data-ng-repeat='tick in ticks' ng-bind-html='tick|trust'></li></ul>",
+    template:"<div class='promo-ticker'><ul><li data-ng-repeat='tick in ticks' ng-bind-html='tick|trust'></li></ul><div>",
     link: function(scope, element, attrs, autoCtrl){
       scope.$watch('ticks',function(val){
         if(val){
-          jQuery(element).vTicker();
+          $timeout(function(){
+            jQuery(element).find('.promo-ticker').vTicker();
+          },500);
         }
       });
     }
